636bb0c173c86306bded0476f67810b436435b2a
Exzzy911 <141855192+Exzzy911@users.noreply.github.com>
2024-01-09 12:20:12 +0545

    updated

---
+++ script.js
@@ -0,0 +1,20 @@
+// NOT Logical Operator
+
+let temp = 50;
+let sun = true;
+
+
+if(!(temp > 0)){
+    console.log("Temperature is warm");
+}
+else{
+    console.log("Temperature is bad");
+}
+if(!sun){
+    console.log("It is sunny");
+}
+else{
+    console.log("It's not sunny");
+}
+
+
